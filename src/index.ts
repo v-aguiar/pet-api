63d4c28875d91d573affd8ac2ad824efe484cff4
v-aguiar <v.aguiar.dev@gmail.com>
2022-08-07 20:08:49 -0300

    config: initial project structure and dependencies

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+﻿import "express-async-errors";
+import express, { json } from "express";
+
+import cors from "cors";
+import router from "./routes/router.js";
+
+const server = express();
+
+server.use(json());
+server.use(cors());
+
+server.use(router);
+
+export default server;
---
+++ src/server.ts
@@ -0,0 +1,15 @@
+﻿import chalk from "chalk";
+import dotenv from "dotenv";
+
+import server from "./index.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+  console.log(
+    chalk.bold.greenBright("\n🚀 Server is running!") +
+      chalk.bold.cyanBright("\n\nListening on port " + PORT + "...\n")
+  );
+});
